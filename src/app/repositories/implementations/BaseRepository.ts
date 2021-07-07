eb4bd9be075d81e915b8ff9452a0d153e105b225
KiwiDev808 <lucasbacela@hotmail.com>
2021-07-07 15:43:30 -0300

    refactor:renamed databaseconnection and moved it to repositories folder

---
+++ src/app/repositories/implementations/BaseRepository.ts
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import { default as knex, Knex } from 'knex';
+import { DB_HOST, DB_PASSWORD, DB_PORT, DB_SCHEMA, DB_USER } from '../config';
+
+dotenv.config();
+
+export class DatabaseConnection {
+  private static dbConnection: Knex;
+  protected static connection(): Knex {
+    if (!this.dbConnection) {
+      this.dbConnection = knex({
+        client: 'mysql',
+        connection: {
+          host: DB_HOST,
+          user: DB_USER,
+          password: DB_PASSWORD,
+          database: DB_SCHEMA,
+          port: DB_PORT || 3306,
+          multipleStatements: true,
+        },
+      });
+    }
+
+    return this.dbConnection;
+  }
+
+  protected static async destroy(): Promise<void> {
+    if (this.dbConnection) {
+      await this.dbConnection.destroy();
+    }
+    return;
+  }
+}
